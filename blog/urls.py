4a86c8f2a9ddeb4335bdd1e38d7b1708aac46819
Alice-space <lizhihao0525@gmail.com>
2019-11-05 10:41:54 +0800

    redo

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .viewsList import generateList
-from .viewsDetail import generateDetail
-
-urlpatterns = [
-    path('article/<int:articleId>', generateDetail),
-    path('<str:listColumn>/<int:listId>', generateList),
-]
